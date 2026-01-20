6288f619c01efbb6560b265fe69b41626baabcc1
Adam Elí <adameli22@gmail.com>
2026-01-20 12:41:50 +0000

    Add tailwindcss and configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig, envField } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    plugins: [tailwindcss()],
     server: {
       watch: {
         usePolling: true,
